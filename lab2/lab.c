99e13b5f3201272d1872acd215aa77bb95920948
anatol77792 <anatol77792@rambler.ru>
2019-07-17 00:22:26 +0700

    Доделана

--- lab2/lab.c
+++ lab2/lab.c
@@ -3,86 +3,98 @@
 #include <string.h>
 #define MAX_LEN 1024 
  
-int inp_str(char *string, int maxlen){
+int inp_str(char *string, int maxlen) {
     char buffer[MAX_LEN];
-    scanf("%[^\n]", buffer); 
+    scanf("%[^\n]", buffer);
     getchar();
-    strncpy(string, buffer, maxlen); 
-    size_t len = strlen(buffer);
-   
-    return (int)len;
+    strncpy(string, buffer, maxlen);
+    return 0;
 }
- 
-char** readMas(int count, char **mas, int maxlen){
-    for (int i = 0; i < count ; i++){
-        mas[i] = (char *)malloc(sizeof(char)*maxlen); 
+
+char** readMas(int count, char **mas, int maxlen) {
+    for (int i = 0; i < count ; i++) {
+        mas[i] = (char *)malloc(sizeof(char)*maxlen);
         inp_str(mas[i], maxlen);
     }
     return mas;
 }
- 
-void printMas(char **mas, int count){
-    for (int i = 0; i < count ; i++){
+
+void printMas(char **mas, int count) {
+    for (int i = 0; i < count ; i++) {
         printf("%s\n", mas[i]);
     }
 }
- 
-void freeMas(char **mas, int count){
-    for (int i = 0; i < count; i++){
+
+void freeMas(char **mas, int count) {
+    for (int i = 0; i < count; i++) {
         free(mas[i]);
     }
     free(mas);
 }
- 
-int getLenFirst(char *str){
+
+int kolslov(char *str) {
     int len = 0;
     for (int i = 0; i < strlen(str); i++)
     {
-        if (str[i]!='0'){
+        if (str[i]==' ') {
             len++;
-        } else{
-            break;
+        } else {
+            ;
         }
     }
     return len;
 }
- 
 
- 
-int bubleSort(char **mas, int n){
- int c = 0;
- int d = 0;
- int swichcount = 0;
- char *swap;
-  for (c = 0 ; c < ( n - 1 ); c++){
-    for (d = 0 ; d < n - c - 1; d++){
-      if (getLenFirst(mas[d+1]) > getLenFirst(mas[d])){
-        swap     = mas[d];
-        mas[d]   = mas[d+1];
-        mas[d+1] = swap;
-        swichcount++;
-      }
+int maxkolslov (char *str1) {
+    int len1 = 0;
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        if (str1[i]==' ') {
+            len1++;
+        } else {
+            ;
+        }
     }
-  }
-  return swichcount;
+    return len1+1;
 }
- 
-int main(int argc, char **argv){
-    char **mas = NULL; //указатель на массив указателей на строки
-    int count = 3;
-    mtrace();
+
+
+
+int bubleSort(char **mas, int n) {
+    int c = 0;
+    int d = 0;
+    int swichcount = 0;
+    char *swap;
+    for (c = 0 ; c < ( n - 1 ); c++) {
+        for (d = 0 ; d < n - c - 1; d++) {
+            if (kolslov(mas[d+1]) > kolslov(mas[d])) {
+                swap = mas[d];
+                mas[d] = mas[d+1];
+                mas[d+1] = swap;
+
+                swichcount++;
+            }
+        }
+    }
+    return swichcount;
+}
+
+int main(int argc, char **argv) {
+    char **mas = NULL;
+    int count = 0;
+    int mtrace();
     printf("Введите кол-во строк:");
     scanf("%d", &count);
     printf("n=%i\n", count);
     getchar();
-    mas = (char **)malloc(sizeof(char *)*count);// выделяем память для массива указателей
+    mas = (char **)malloc(sizeof(char *)*count);
     mas = readMas(count, mas, MAX_LEN);
     printMas(mas, count);
     int swichcount = bubleSort(mas, count);
- 
+
     printMas(mas, count);
     printf("Кол-во перестановок:");
     printf("%d\n", swichcount);
-     printf ("Длина большей строки «%s» - %ld символов\n", mas[0], strlen (mas[0]) );
+    printf ("Максимальное количество слов «%s» - %d\n", mas[0], maxkolslov(mas[0]) );
     freeMas(mas, count);
 }
